e855561a73612cdf332effa19a1ca958e286a2f6
SENTHILBALAN R P <116082466+SENTHILBALAN07@users.noreply.github.com>
2025-11-24 12:32:02 +0530

    Delete database.py

--- database.py
+++
@@ -1,7 +0,0 @@
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-db_url = "postgresql://postgres:tiger@localhost:5432/senthil"
-engine = create_engine(db_url)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
